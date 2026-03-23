9f1ce42c8a28dace6d5f74a0fb4cda1735f27a17
jiemCode <dmax6176@gmail.com>
2026-03-23 13:24:11 +0000

    first commit

---
+++ src/main/java/com/fleety/olympics/OlympicMedalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fleety.olympics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicMedalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympicMedalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleety/olympics/OlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleety.olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicMedalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
