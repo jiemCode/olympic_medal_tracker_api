fdab7b5cb8411540bc4679f45224ca874acdd7d0
jiemCode <dmax6176@gmail.com>
2026-03-26 09:54:44 +0000

    added builder patterns

--- src/main/java/com/fleety/olympics/model/Competition.java
+++ src/main/java/com/fleety/olympics/model/Competition.java
@@ -10,10 +10,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "competitions")
 public class Competition {
 
